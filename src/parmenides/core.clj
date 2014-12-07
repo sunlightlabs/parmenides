ae7898ccf36d9a68e5ec2cd89b8292a49935a1a0
Zack Maril <zack@zacharymaril.com>
2014-12-07 01:25:13 -0500

    Db functions,  attribute descriptors, beings
    * Start of continues resolution process function
    * Attribute descriptions like forward ownership, reverse ownership,
      multiplicity and reliable to be used for entity resolution.
    * Bringing over some of the framework from echelon for tracking beings

--- src/parmenides/core.clj
+++ src/parmenides/core.clj
@@ -1,57 +1,118 @@
 (ns parmenides.core
-  (:require [clojure.test.check :as tc]
-            [clojure.test.check.generators :as gen]
-            [clojure.test.check.properties :as prop]
-            [clojure.test.check.clojure-test :as ct :refer (defspec)]
-            [datomic.api :as d :refer [db q]]))
+  (:require [datomic.api :as d]))
+
+
 
 (def parmenides-attributes
-  [{:db/id #db/id[:db.part/db]
-    :db/ident :parmenides/record-type
+  [{:db/ident :parmenides.record/type
     :db/valueType :db.type/ref
     :db/cardinality :db.cardinality/one
-    :db.install/_attribute :db.part/db}])
+    :db.install/_attribute :db.part/db
+    :db/id #db/id[:db.part/db]}
 
-(def test-attributes
-  [{:db/id #db/id[:db.part/db]
-    :db/ident :id
-    :db/valueType :db.type/long
+   {:db/ident :parmenides.record/represents
+    :db/valueType :db.type/ref
     :db/cardinality :db.cardinality/one
-    :db.install/_attribute :db.part/db}])
+    :db.install/_attribute :db.part/db
+    :db/id #db/id[:db.part/db]}
 
+   {:db/ident :parmenides.being/tenuously-connected
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one
+    :db.install/_attribute :db.part/db
+    :db/id #db/id[:db.part/db]}
 
-(defn get-fresh-dbc [random-postfix]
-  (let [url (str "datomic:mem://parmenides" random-postfix)
-        conn (do (d/create-database url)
-                 (d/connect url))]
+   {:db/ident :parmenides.being/id
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db.install/_attribute :db.part/db
+    :db/id #db/id[:db.part/db]}
 
-    (d/transact conn parmenides-attributes)
-    (d/transact conn test-attributes)
-    conn))
+   {:db/ident :parmenides/individual :db/id #db/id[:db.part/user]}
 
-(resolve-log)
-(resolve-range)
-(contiually-resolve-transactions)
-;; full-resolve scans the entire database
-;; cont
+   {:db/ident :parmenides.attributes/forward-ownership
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one
+    :db/doc
+    "The number of entities that can share ownership of the value of
+    this attribute. The first option is individual ownership, like a
+    government assigned identifier. The second option is shared
+    ownership, like a home address or phone number for a family. The
+    last option is nobody, in that nobody can own a value, i.e. a type
+    of organization, a color or particular make of a car."
+    :db.install/_attribute :db.part/db
+    :db/id #db/id[:db.part/db]}
 
-(def simple-test
-  (prop/for-all
-   [random-postfix (gen/not-empty gen/string-alpha-numeric)
-    v (gen/vector gen/int)
-    n gen/nat]
-   (let [conn (get-fresh-dbc random-postfix)]
-     (full-resolve conn)
-     (d/transact conn (take n (cycle [{:id 1} {:id 2}])))
+   {:db/ident :parmenides.attributes/reverse-ownership
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one
+    :db/doc
+    "When the attribute is a reference to another entity, reverse
+    ownership indicates the number of child entities that can share
+    ownership of the parent entity. The first option is individual
+    ownership, like in the instance where a child entity is the sole
+    owner of the parent entity which is a sole proprietorship. The
+    second option is shared ownership, like the child entities are
+    employees of the parent entity which is a large organization. The
+    last option, the default option, is nobody, in that none of the
+    children can own the parent. Unsure whether this is actually
+    needed or not but seems potenially useful."
+    :db.install/_attribute :db.part/db
+    :db/id #db/id[:db.part/db]}
 
-     (= n (count (d/q '[:find ?e
-                        :where [?e :id]]
-                      (db conn))))
+   {:db/ident :parmenides.ownership/individual :db/id #db/id[:db.part/user]}
+   {:db/ident :parmenides.ownership/shared :db/id #db/id[:db.part/user]}
+   {:db/ident :parmenides.ownership/nobody :db/id #db/id[:db.part/user]}
 
-     (= n (count (d/q '[:find ?e
-                        :where [?e :id]]
-                      (db conn))))
+   {:db/ident :parmenides.attributes/multiplicity
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one
+    :db/doc "How many different values should an entity own for this
+    attribute? The first option is one and the second option is
+    many. A person should only have one social security but might have
+    many different driver's license numbers."
+    :db.install/_attribute :db.part/db
+    :db/id #db/id[:db.part/db]}
 
-     (= (sort v) (sort (sort v))))))
+   {:db/ident :parmenides.multiplicity/one :db/id #db/id[:db.part/user]}
+   {:db/ident :parmenides.multiplicity/many :db/id #db/id[:db.part/user]}
 
-(tc/quick-check 10 simple-test)
+   {:db/ident :parmenides.attributes/reliable
+    :db/valueType :db.type/boolean
+    :db/cardinality :db.cardinality/one
+    :db/doc
+    "Is the value of this attribute reliably entered? Pessimistically,
+    the default is false."
+    :db.install/_attribute :db.part/db
+    :db/id #db/id[:db.part/db]}
+
+   ;;; Built in attributes that are handled as special cases
+   {:db/ident :parmenides.person/name
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db.install/_attribute :db.part/db
+    :db/id #db/id[:db.part/db]}
+
+   {:db/ident :parmenides.organization/name
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db.install/_attribute :db.part/db
+    :db/id #db/id[:db.part/db]}
+
+   {:db/id #db/id [:db.part/user]
+    :db/ident :parmenides.resolution/continuous
+    :db/fn #db/fn {:lang "clojure"
+                   :params [conn]
+                   :requires [[clojure.core.async :refer [go-loop]]
+                              [datomic.api :as d]]
+                   :code (let [queue (d/tx-report-queue conn)]
+                           (go-loop []
+                             (seq (:tx-data (.poll queue)))
+                             (println "Got Data")))}}])
+
+(defn continous-resolution [conn]
+  (-> conn
+      d/db
+      (d/entity :parmenides.resolution/continuous)
+      :db/fn
+      (.invoke conn)))
--- test/parmenides/core_test.clj
+++ test/parmenides/core_test.clj
@@ -1,7 +1,54 @@
 (ns parmenides.core-test
   (:require [clojure.test :refer :all]
-            [parmenides.core :refer :all]))
+            [parmenides.core :refer :all]
+            [clojure.test.check :as tc]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :as ct :refer (defspec)]
+            [datomic.api :as d :refer [db q]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(def test-attributes
+  [{:db/id #db/id[:db.part/db]
+    :db/ident :test/id
+    :db/valueType :db.type/long
+    :db/cardinality :db.cardinality/one
+    :db.install/_attribute :db.part/db
+    ;:parmenides/ownership :parmenides/individual
+    ;:parmenides/reliable true
+    ;:parmenides/multiplicity :parmendies/one
+    }
+   {:db/ident :test/record :db/id #db/id[:db.part/user]}])
+
+
+(defn get-fresh-conn [s]
+  (let [url (str "datomic:mem://parmenides" s)
+        conn (do (d/create-database url)
+                 (d/connect url))]
+    (d/transact conn parmenides-attributes)
+    (d/transact conn test-attributes)
+    conn))
+
+(defspec simple-test
+  1
+  (prop/for-all
+   [n gen/nat
+    random-postfix (gen/such-that #(< 2 (count %)) gen/string-alpha-numeric)]
+
+   (let [conn (get-fresh-conn random-postfix)
+         datoms (repeatedly n #(hash-map :test/id 1
+                                         :db/id (d/tempid :db.part/user)
+                                         :parmenides/record-type :test/record))]
+     (continous-resolution conn)
+     (d/transact conn datoms)
+     (is (= n (count (d/q '[:find ?e
+                            :where [?e :test/id ?v]]
+                          (db conn)))))
+     (is (= 1 (count (d/q '[:find ?e
+                            :where [?e :parmenides.being/id ?v]]
+                          (db conn)))))
+
+
+     )))
+
+(run-tests)
