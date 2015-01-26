425b1f8acc0a1b1d51521bc167fc08520c4abc78
Ubuntu <ubuntu@ip-10-0-0-211.ec2.internal>
2015-01-26 17:35:15 +0000

    Using api-defaults instead of site-defaults
    Doesn't provide much protection but we don't need much as of yet.

--- src/parmenides/core.clj
+++ src/parmenides/core.clj
@@ -2,14 +2,16 @@
   (:require [datomic.api :as d]
             [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
             [aleph.http :as http]))
 
 (declare server)
 
 (defroutes app
-  (GET "/index.html" [] "test"))
+  (GET "/index.html" [] "test")
+  (POST "/ingest" [body] (println body) "HELLO PAULTAG SHALL WE PLAY A GAME?")
+  (route/not-found "OH SNAP SON"))
 
 (when (bound? (var server)) (.close server))
 
-(def server (http/start-server (wrap-defaults app site-defaults) {:port 8080}))
+(def server (http/start-server (wrap-defaults app api-defaults) {:port 80}))
