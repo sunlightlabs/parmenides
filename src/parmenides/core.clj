cb39e55e31cd9295810f4b72ce0e366673c8bf05
Zack Maril <zack@zacharymaril.com>
2015-01-28 21:03:01 +0000

    (Should actually commit the files)

--- src/parmenides/core.clj
+++ src/parmenides/core.clj
@@ -6,26 +6,24 @@
             [aleph.http :as http]
             [schema.core :as s]
             [parmenides.ocd :refer [OCD]]
-            [clojure.data.json :refer [read-str]
-                                        ;[clojure.java.shell :refer [sh]]
-             ]))
-
+            [clojure.data.json :refer [read-str]]
+            [taoensso.timbre :as timbre]))
+(timbre/refer-timbre)
 
 (def last-requests (atom []))
-
 (defn ingest [{body :body params :params}]
-  ;;(println (str params) (quot (System/currentTimeMillis) 1000))
-  (when-not (:save params)
-    (let [filename (str "example-data/"(str (d/squuid)))]
-      (spit filename (slurp body))
-      (println filename)))
-  (let [{data "data" apikey "apikey"} (read-str (slurp body))]
-    (println apikey)
-    (swap! last-requests concat data)))
+  (info "recieved file")
+  (let [body (slurp body)]
+    (when-not (:save params)
+      (let [filename (str "example-data/"(str (d/squuid)))]
+        (spit filename body)
+        (info filename)))
+    (let [{data "data" apikey "api-key"} (read-str body)]
+      (swap! last-requests concat data))))
 
 (defroutes app
   (GET "/index.html" [] "test")
-  (POST "/ingest" request (ingest request) "HELLO PAULTAG SHALL WE PLAY A GAME?\n")
+  (POST "/ingest" request (ingest request) "*burp*\n")
   (route/not-found "OH SNAP SON"))
 
 (declare server)
@@ -33,5 +31,9 @@
 
 (def server (http/start-server (wrap-defaults app api-defaults) {:port 80}))
 
-(count (clojure.java.shell/sh "./test.sh"))
+#_(count (clojure.java.shell/sh "./smalltest.sh"))
+#_(count (clojure.java.shell/sh "./fulltest.sh"))
+
+
 (count (map (partial s/validate OCD) @last-requests))
+1
